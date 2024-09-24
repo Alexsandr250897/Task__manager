6dea04a99b49f8255ad51da051ac7b1ad52730dc
Alexsandr250897 <132817845+Alexsandr250897@users.noreply.github.com>
2024-09-24 18:53:50 +0200

    Изменение файла entrypoint

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ COPY pyproject.toml poetry.lock ./
 RUN poetry export -f requirements.txt | pip install -r /dev/stdin --no-deps
 EXPOSE 8000
 COPY ./entrypoint.sh ./
-ENTRYPOINT ["sh", "./entrypoint.sh"]
+RUN chmod +x entrypoint.sh
 COPY . ./
-CMD ./entrypoint.sh 
+CMD ./entrypoint.sh && python manage.py runserver 0.0.0.0:8000
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
+fi
 python manage.py runserver 0.0.0.0:8000
